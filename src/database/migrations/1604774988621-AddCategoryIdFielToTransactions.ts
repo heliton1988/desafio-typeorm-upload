f6d1b40fd94230ae2684988ed9fc642f411d45bb
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-11-08 16:45:43 -0300

    :sparkles: challenge done

---
+++ src/database/migrations/1604774988621-AddCategoryIdFielToTransactions.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCategoryIdFielToTransactions1604774988621
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'categoryTransaction',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'categoryTransaction');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
